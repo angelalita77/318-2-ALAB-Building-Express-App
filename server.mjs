be2bd8085eda6391505b3aa168784700f8bcf7c4
angelalita77 <anjeanette@gmail.com>
2025-09-03 17:13:55 -0400

    successful with express server inital setup. Listen port completed, GET route successful

--- server.mjs
+++ server.mjs
@@ -1,12 +1,22 @@
 //Import (1)
+import express from "express";
+
+
 
 //Setup (2)
+const app = express();
+const PORT = 3000;
 
 
 //Middleware
 
 //Routes
-
+app.get("/", (req, res) => {
+    res.send("You found me bro!");
+})
 //Global Error Hardling Middleware
 
 //Server Listener (3)
+app.listen(PORT, (req, res) => {
+    console.log(`Successfully connected through port ${PORT}`);
+})
