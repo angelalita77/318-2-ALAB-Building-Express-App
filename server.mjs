ee5435776877af71fa887381cd16867c50761d1f
angelalita77 <anjeanette@gmail.com>
2025-09-04 00:53:17 -0400

    installed ejs, created app.routes

--- server.mjs
+++ server.mjs
@@ -9,11 +9,40 @@ const PORT = 3000;
 
 
 //Middleware
+// app.use(logReq);
+
+
+// function logReq(req, res, next){
+//     console.log(`${req.method} request made to '${req.url}'`);
+//     next();
+// }
 
 //Routes
-app.get("/", (req, res) => {
-    res.send("You found me bro!");
+app
+.route('/horror')
+.get((req, res) => {
+    res.send("The Ring");
+    console.log(req.params.id)
 })
+.post( (req, res) => {
+    res.send(`New Title Added: "Skinamarink" `);
+})
+.put((req, res) => {
+    res.send(`Title Updated: "The Ring (US)"`)
+})
+.delete( (req, res) => {
+    res.send(`Title Deleted: "The Ring (US)"`)
+})
+
+app
+.route("/horror/:id")
+.put((req,res) => {
+    res.send(`Testing PUT Param value ${req.params.id}.`)
+})
+.delete( (req, res) => {
+    res.send(`Testing DELETE Param value ${req.params.id}"`)
+});
+
 //Global Error Hardling Middleware
 
 //Server Listener (3)
